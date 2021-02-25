d6c10890dd37479f051c4fa5b5c4f804c0fcef94
alex <alex@gmail.com>
2021-02-25 00:12:16 -0500

    "primer commit"

---
+++ src/pe/gob/conexion/Demo.java
@@ -0,0 +1,22 @@
+package pe.gob.conexion;
+
+public class Demo {
+	
+	private int codcategoria;
+	private String descategoria;
+	
+	public int getCodcategoria() {
+		return codcategoria;
+	}
+	public void setCodcategoria(int codcategoria) {
+		this.codcategoria = codcategoria;
+	}
+	public String getDescategoria() {
+		return descategoria;
+	}
+	public void setDescategoria(String descategoria) {
+		this.descategoria = descategoria;
+	}
+	
+	
+}
